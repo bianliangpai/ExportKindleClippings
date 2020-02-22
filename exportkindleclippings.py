d7d2a62f6210cf86dbbbbf55fcbd0bf998794294
bianliangpai <bianliangpai@gmail.com>
2020-02-22 10:21:30 +0800

    [NEW]: add comments and unittest

--- exportkindleclippings.py
+++ exportkindleclippings.py
@@ -1,5 +1,9 @@
 import sys
 
+def print_usage():
+    print('usage:')
+    print('  python exportkindleclippings.py path/to/My Clippings.txt\n')
+
 def is_similarly(note1 = '', note2 = ''):
     if len(note1) >= len(note2):
         return note1[:len(note2)] == note2
@@ -13,6 +17,8 @@ def readclippings(clippingspath = 'My Clippings.txt'):
 
     while True:
         onenote=[]
+        # kindle默认每一条笔记会在'My Clippings.txt'里产生5行记录
+        # 其中第1行为文件名，第4行为实际需要的记录
         for _ in range(0,5):
             line = f.readline()
             if not line:
@@ -20,17 +26,18 @@ def readclippings(clippingspath = 'My Clippings.txt'):
             line = line.rstrip('\n')
             onenote.append(line)
 
-        # put into dict
         curnote = onenote[3]
         bookpath = onenote[0]+'.txt'
         if not clippingnotes.get(bookpath):
             clippingnotes[bookpath] = []
         if not clippingnotes[bookpath]:
             clippingnotes[bookpath].append(curnote)
-        elif not is_similarly(clippingnotes[bookpath][-1], curnote):
-            clippingnotes[bookpath].append(curnote)
-        else:
+        # 在kindle内选择笔记时，由于跨页、系统卡顿等原因，笔记的选择范围经常会不太精确
+        # 由于kindle会把所有误操作的笔记也写进My Clippings.txt里，为方便整理，这里去掉了这些重复记录
+        elif is_similarly(clippingnotes[bookpath][-1], curnote):
             clippingnotes[bookpath][-1] = curnote
+        else:
+            clippingnotes[bookpath].append(curnote)
             
 
 
@@ -43,6 +50,8 @@ def writenotes(clippingnotes = {}):
 
 
 if __name__ == "__main__":
+
+    print_usage()
 
     if len(sys.argv) > 1:
         clippingnotes = readclippings(sys.argv[1])
